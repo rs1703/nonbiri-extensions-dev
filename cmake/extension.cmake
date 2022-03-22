9b181a80597e9a4bd51e8cafa0c623c37694826b
rs <96896020+rs1703@users.noreply.github.com>
2022-03-22 15:52:33 +0700

    fix extension useApi always true

--- cmake/extension.cmake
+++ cmake/extension.cmake
@@ -5,6 +5,6 @@ target_compile_definitions(
     BASE_URL="${baseUrl}"
     LANGUAGE="${language}"
     VERSION="${version}"
-    USE_API="${useApi}"
+    USE_API=${useApi}
 )
 target_compile_features(${id} PRIVATE cxx_std_17)

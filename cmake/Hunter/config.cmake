ac8c88d678d24dd0c60181251c82417eecca4c4b
rs <96896020+rs1703@users.noreply.github.com>
2022-03-25 23:12:57 +0700

    [skip ci] update cmake configurations

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,26 @@
+hunter_config(
+  OpenSSL
+  VERSION 1.1.1j
+)
+
+hunter_config(
+  gumbo
+  VERSION 0.10.1
+)
+
+hunter_config(
+  jsoncpp
+  VERSION 1.8.0
+)
+
+hunter_config(
+  CURL
+  VERSION 7.60.0-p2
+  CMAKE_ARGS
+    CMAKE_USE_SCHANNEL=OFF
+    BUILD_CURL_TESTS=OFF
+    BUILD_CURL_EXE=OFF
+    CMAKE_USE_OPENSSL=ON
+    CMAKE_USE_LIBSSH2=OFF
+    BUILD_TESTING=OFF
+)

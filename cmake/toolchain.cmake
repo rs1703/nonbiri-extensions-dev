731d658672bd853e4faf89bd567d36ec4d464347
rs <96896020+rs1703@users.noreply.github.com>
2022-04-20 14:04:44 +0700

    update cmake configurations

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -1,9 +1,43 @@
-set(CMAKE_POSITION_INDEPENDENT_CODE TRUE)
-
+set(CMAKE_C_COMPILER gcc)
 set(CMAKE_CXX_COMPILER g++)
 set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED YES)
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -O3")
+set(CMAKE_POSITION_INDEPENDENT_CODE TRUE)
 
-set(CMAKE_C_COMPILER gcc)
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -O3")
+find_program(CCACHE_FOUND ccache)
+if(CCACHE_FOUND)
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache)
+endif(CCACHE_FOUND)
+
+if(WIN32)
+	set(
+		CMAKE_CXX_FLAGS
+		"${CMAKE_CXX_FLAGS} \
+    /Os
+		/Wall \
+		/pipe \
+		/pedantic \
+		/fsized-deallocation \
+    /ffunction-sections \
+    /fdata-sections \
+		/fdiagnostics-color=always")
+else()
+	set(
+		CMAKE_CXX_FLAGS
+		"${CMAKE_CXX_FLAGS} \
+    -Os \
+		-Wall \
+		-Wextra \
+		-pipe \
+		-pedantic \
+		-fsized-deallocation \
+    -ffunction-sections \
+    -fdata-sections \
+		-fdiagnostics-color=always \
+		-Wunreachable-code \
+		-Wno-attributes")
+	if(NOT CMAKE_COMPILER_IS_GNUCXX)
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wshadow")
+	endif()
+endif()

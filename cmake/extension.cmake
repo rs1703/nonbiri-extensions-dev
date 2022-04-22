71ff78f078a96f76530393592412f0118b8e8973
rs <96896020+rs1703@users.noreply.github.com>
2022-04-23 03:56:21 +0700

    add isNsfw

--- cmake/extension.cmake
+++ cmake/extension.cmake
@@ -5,6 +5,7 @@ target_compile_definitions(
     BASE_URL="${baseUrl}"
     LANGUAGE="${language}"
     VERSION="${version}"
+    IS_NSFW=${isNsfw}
     USE_API=${useApi}
 )
 target_compile_features(${id} PRIVATE cxx_std_17)
